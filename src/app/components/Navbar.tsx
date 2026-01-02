4b9cde2fdb2530949dfb07796ec169f3be71c530
Swayam Rajput <swayamrajput029@gmail.com>
2026-01-02 17:39:29 +0530

    feat: added lucide icons for theme toggling

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,8 @@
 import Link from "next/link";
 import { useState, useRef, useEffect, ReactElement } from "react";
 import Cat from "./Cat";
-
+import { SunIcon } from "@/components/ui/sun";
+import { MoonIcon } from "@/components/ui/moon";
 export const Navbar = () => {
     const [isDark,setDark] = useState(false);
     const [hoverStyle, setHoverStyle] = useState({ left: 0, width: 0, opacity: 0 });
@@ -79,7 +80,7 @@ export const Navbar = () => {
                             </li> */}
                         </ul>
                         <div className="flex justify-end flex-row-reverse gap-2 ">
-                            <button onClick={toggleMode} onMouseEnter={handleMouseEnter} className="transition  hover:text-accent-foreground duration-300 rounded-sm px-2 py-2">
+                            {/* <button onClick={toggleMode} onMouseEnter={handleMouseEnter} className="transition  hover:text-accent-foreground duration-300 rounded-sm px-2 py-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" >
                                     {
                                         (!isDark)?
@@ -91,6 +92,14 @@ export const Navbar = () => {
                                     
                                     }
                                 </svg>
+                            </button> */}
+                            <button onClick={toggleMode} onMouseEnter={handleMouseEnter} className="transition  hover:text-accent-foreground duration-300 rounded-sm px-2 py-2">
+                                {
+                                    (!isDark)?
+                                    <SunIcon/>:
+                                    <MoonIcon/>
+                                }
+                                
                             </button>
                             <button onMouseEnter={handleMouseEnter} onClick={()=>{setCatShown(prev=>!prev)}} className="transition  hover:text-accent-foreground duration-300 rounded-sm px-1 py-2">
                                 
