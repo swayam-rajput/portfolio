83740825951140dc44f0776c41dd938fef97b5db
Swayam Rajput <swayamrajput029@gmail.com>
2026-02-17 00:35:10 +0530

    achromatic github theme added but commented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,11 @@ import React from "react";
 const GithubCalendarStats = ({username}:{username:string}) => {
     const { theme } = useTheme();
     const github_theme = {
-        light: ["#f5f5f5", "#e5e5e5", "#d4d4d4", "#a3a3a3", "#737373"],
-dark:  ["#1f1f1f", "#2a2a2a", "#3a3a3a", "#525252", "#737373"],
+        // light: ["#f5f5f5", "#e5e5e5", "#d4d4d4", "#a3a3a3", "#737373"],
+        // dark:  ["#1f1f1f", "#2a2a2a", "#3a3a3a", "#525252", "#737373"],
 
-        // light: ["#eef7f1", "#cfe9d6", "#9fd5b2", "#5fbf86", "#2e8f5a"],
-        // dark:  ["#202020", "#163524", "#1f5c3d", "#2f8a5a", "#4fc27f"],
+        light: ["#eef7f1", "#cfe9d6", "#9fd5b2", "#5fbf86", "#2e8f5a"],
+        dark:  ["#202020", "#163524", "#1f5c3d", "#2f8a5a", "#4fc27f"],
     };
     return (
         <GitHubCalendar renderBlock={(block) => {
