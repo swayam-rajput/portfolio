f21dccb800a69281cbf7dc4a0c35fa6b1ccd8b2b
Swayam Rajput <swayamrajput029@gmail.com>
2026-05-15 12:23:01 +0530

    added my name

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,12 +8,13 @@ const Footer = () => {
     return (
         
         <div className=" flex justify-center items-center flex-col-reverse sm:flex-row gap-6 sm:justify-between px-4 my-4 top-0 py-6">
-            <p className=" flex flex-col text-center text-xs text-muted-foreground">
+            <span className=" flex flex-col font-outfit text-[13px] text-muted-foreground">
+                <span>Designed by <p className="animate-underline text-foreground pb-0.5 font-medium">Swayam</p></span>
                 <span>© {new Date().getFullYear()}</span> 
-            </p>
+            </span>
             {/* <Image alt="" width={73} height={81} src={"/summer.png"}/> */}
             <Link className="py-4 opacity-85 hover:opacity-100 active:opacity-50 text-sm transition-all" href={'/contact'}>
-                <Button variant={"link"} className="inline-flex  px-3 py-0.5 transition-all duration-200 shadow-white/20 shadow-xl hover:shadow-none ">
+                <Button variant={"link"} className="inline-flex  px-3 py-0.5 transition-all duration-200  ">
                     Contact Me
                 </Button>
             </Link >
