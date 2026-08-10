e4d718f792f4df3f4968a30283601383dcb262f4
Swayam Rajput <swayamrajput029@gmail.com>
2026-08-10 22:59:28 +0530

    style(socials/m-tooltip): removed leetcode icon and added Cal

--- src/app/components/Socials.tsx
+++ src/app/components/Socials.tsx
@@ -16,16 +16,15 @@ export const Socials = () => {
                 </svg>
             </a>
             </MinimalTooltip>
-            <MinimalTooltip text="Leetcode" position="bottom">
             
-            <a href="https://leetcode.com/u/swayam_rajput" target="_blank" className="group-hover:opacity-40 hover:!opacity-100 transition text-muted-foreground opacity-60" rel="noopener noreferrer" title="Leetcode"><span className="sr-only">Leetcode</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 1 24 24" fill="none" className="stroke-current" role="img" stroke="currentColor" >
-                    <path d="M14 3L4 13L12 21L16 17" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-                    <path d="M14 3L12 5M4 13L12 5M12 5L16 9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-                    <path d="M11 13H20" strokeWidth="2" strokeLinecap="round"></path>
-                </svg>
+            <MinimalTooltip text="Cal" position="bottom">
+            <a href="https://cal.com/samrajput" target="_blank" className="group-hover:opacity-40 hover:!opacity-100 transition text-muted-foreground opacity-60" rel="noopener noreferrer" title="Cal"><span className="sr-only">Cal</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" strokeWidth={3} stroke="currentColor"   viewBox="0 0 256 256"><path d="M208,32H184V24a8,8,0,0,0-16,0v8H88V24a8,8,0,0,0-16,0v8H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM72,48v8a8,8,0,0,0,16,0V48h80v8a8,8,0,0,0,16,0V48h24V80H48V48ZM208,208H48V96H208V208Zm-68-76a12,12,0,1,1-12-12A12,12,0,0,1,140,132Zm44,0a12,12,0,1,1-12-12A12,12,0,0,1,184,132ZM96,172a12,12,0,1,1-12-12A12,12,0,0,1,96,172Zm44,0a12,12,0,1,1-12-12A12,12,0,0,1,140,172Zm44,0a12,12,0,1,1-12-12A12,12,0,0,1,184,172Z"></path></svg>
             </a>
             </MinimalTooltip>
+            
+
+
         </section>
     )
 }
